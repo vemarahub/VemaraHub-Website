258f154a2658f13c4509d644dd9c87f11470e5ff
vemarahub <vemarahub@gmail.com>
2020-06-27 16:18:04 +0530

    without http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ec2-13-233-64-121.ap-south-1.compute.amazonaws.com/api/'
+  apiUrl: 'ec2-13-233-64-121.ap-south-1.compute.amazonaws.com/api/'
 };
